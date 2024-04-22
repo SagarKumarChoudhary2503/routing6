7f371503c5b28b2a561b0a2aac4c8b03d46544ff
Sagar Choudhary <skchoudhary2503@gmail.com>
2024-04-22 08:42:39 +0530

    my work

---
+++ src/component/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return(
+        <div>
+            <hr/>
+            <center>
+                <h3>&copy; Sagar Developer 2024</h3>
+            </center>
+        </div>
+
+    )
+}
+
+export default Footer;
